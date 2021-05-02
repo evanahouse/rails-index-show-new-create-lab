23f3e2f8ffcd8f8eac069158b3ee50a89fdeb680
Evan <evanahouse@gmail.com>
2021-05-01 21:44:55 -0700

    completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.destroy_all
+
+Coupon.create(coupon_code:'ABCDEF', store: 'Lowes')
+Coupon.create(coupon_code:'HDJSKC', store: 'Arbys')
+Coupon.create(coupon_code:'BDJVID', store: 'Chipotle')
+Coupon.create(coupon_code:'LDJEUC', store: 'McDonalds')
+Coupon.create(coupon_code:'QWERTY', store: 'Target')
